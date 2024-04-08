bf01a3e6c1e39e0fcca25ded8602b21c5673079f
aruzhan <atelova_aruzhann@mail.ru>
2024-04-08 14:42:00 +0500

    creted repositorie1

--- src/myaplication.java
+++ src/myaplication.java
@@ -1,4 +1,19 @@
 public class myaplication {
+    public static void main() {
+        int[] arr=new int[] {5,2,5,23,23,78,25,89,88};
+                int x=minValue(arr);
+        System.out.println(x);
+    }
+
+    public static int minValue(int[] array) {
+        int currentMinimum = array[0];
+        for (int i : array) {
+            if (i < currentMinimum) {
+                currentMinimum = i;
+            }
+        }
+        return currentMinimum;
+    }
 
 
 }
